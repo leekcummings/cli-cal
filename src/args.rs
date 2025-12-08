c2c86909e2444510b265861793a7d3264ec9bd2d
leekcummings <absoluteleefine@gmail.com>
2025-12-08 15:04:35 -0500

    minor tweaks, adding bin settigns to Cargo.toml

--- src/args.rs
+++ src/args.rs
@@ -6,12 +6,6 @@ use clap:: {
     ArgAction
 };
 
-// Not using this I think unless I can get multiple clap arguments into here at once
-// fn date_parse(raw_date: &str) -> Result<chrono::DateTime<Utc>, anyhow::Error> {
-//     println!("{}", &raw_date);
-//     parse_with(raw_date, &Utc, DEFAULT_TIME)
-// }
-
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct CalArgs {
@@ -23,7 +17,7 @@ pub struct CalArgs {
 pub enum EventOptions {
     /// Add event to your calendar
     Add(AddArgs),
-    /// Edit an existing event in calendar
+    /// Edit an existing event in calendar (currently not working)
     // Use wild args to find name using regex
     // https://crates.io/crates/wild
     Edit(EditArgs),
@@ -35,28 +29,27 @@ pub enum EventOptions {
 pub struct AddArgs {
     // The title of the event
     pub title: String,
-    // The start and end time, separated by a comma
-    #[clap(num_args=1..=10)]
+    #[clap(num_args=1..=10, help="The start and end time, separated by a comma")]
     pub datetimes: Vec<String>,
-    // The location of your event
-    #[clap(short)]
+    #[clap(short, help="The location of your event")]
     pub location: Option<String>,
-    /// Make event daily, weekly, or monthly (currently not working)
-    #[clap(short)]
+    #[clap(short, help="Make event daily, weekly, or monthly (currently not working)")]
     pub repeat: Option<String>,
-    // Add an option description for the event
-    #[clap(short, num_args=1..)]
+    #[clap(short, num_args=1.., help="Add an option description for the event")]
     pub description: Option<String>
 }
 
 #[derive(Debug, Args)]
 pub struct EditArgs {
-
+    // Nothing to show yet...
 }
 
 #[derive(Debug, Args)]
 pub struct ViewArgs {
-    // Show all available events
-    #[clap(short, action=ArgAction::SetTrue)]
-    pub all: Option<bool>
+    #[clap(short, action=ArgAction::SetTrue, help="Show all available events")]
+    pub all: Option<bool>,
+    #[clap(short, action=ArgAction::SetTrue, help="Show event locations")]
+    pub location: Option<bool>,
+    #[clap(short, action=ArgAction::SetTrue, help="Show event repeats")]
+    pub repeat: Option<bool>
 }
